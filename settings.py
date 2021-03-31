33bdae1a600b1251216435ad39412457c1806f61
Neyromancer <neyromant@yandex.ru>
2021-03-31 20:32:42 +0300

    Base module implemented

---
+++ settings.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import os.path
+import sys
+
+def init():
+    global CONFIG_PATH
+    CONFIG_PATH = "/etc/user_settings/user_settings.conf"
+
+    if not os.path.exists( CONFIG_PATH ) or not os.path.isfile( CONFIG_PATH ):
+        print( f"{ CONFIG_PATH } does not exist. Fail to read." )
+        sys.exit( 1 )
+
+    global EXERNAL_MODULES_DIR
+    EXERNAL_MODULES_DIR = "/opt/user_settings/external/"
+
+    if not os.path.exists( EXERNAL_MODULES_DIR ) or not os.path.isdir( EXERNAL_MODULES_DIR ):
+        print( f"{ EXERNAL_MODULES_DIR } does not exist. Fail to load modules." )
+        sys.exit( 1 )
+    elif not os.listdir( EXERNAL_MODULES_DIR ):
+        print( f"Fail to load modules. Directory: { EXERNAL_MODULES_DIR } is empty." )
+        sys.exit( 1 )
+
+    LOG_DIR = "/var/log/user_settings/"
+    if not os.path.exists( LOG_DIR ) or not os.path.isdir( LOG_DIR ):
+        os.makedirs( LOG_DIR, exist_ok = True )
+
+    global LOG_PATH
+    LOG_PATH = os.path.join( LOG_DIR, "user_settings.log" )
---
+++ system_configurator.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from configparser import ConfigParser
+import logging
+import os
+import os.path
+import sys
+import subprocess
+
+import settings
+
+sys.path.insert( 1, "/opt/utils" )
+from user_settings_exceptions import Errors, ModuleNotFoundError, ModuleExecutionError
+
+class UserSettingLoader:
+  def __init__( self ):
+    self.modules_path = settings.EXERNAL_MODULES_DIR
+    self.log = settings.LOG_PATH
+    self.modules = dict()
+
+    logging.basicConfig( filename = self.log, format='%(asctime)-15s %(message)s', level=logging.INFO )
+    self.logger = logging.getLogger( 'user_system_settings' )
+    consoleHandler = logging.StreamHandler()
+    self.logger.addHandler( consoleHandler )
+    self.logger.info( "Set system user configuration" )
+
+  def load_modules( self ):
+    self.logger.info( "Start loading configuration modules" )
+    config = ConfigParser()
+    config.read( settings.CONFIG_PATH )
+
+    modules = config.sections()
+    for module in modules:
+      if module != "BASE" and config[ module ].get( 'enabled', '0' ) == '1':
+        module_name = config[ module ].get( 'name', None )
+        module_path = os.path.join( self.modules_path, module_name )
+        if not os.path.exists( module_path ) or not os.path.isfile( module_path ):
+          self.logger.error( f"Module { module_name } not found" )
+          continue
+
+        module_options = dict()
+        module_options[ 'name' ] = module_name
+        module_options[ 'params' ] = config[ module ].get( 'params', None )
+        self.modules[ config[ module ].get( 'order', 1 ) ] = module_options
+
+      if self.modules:
+        self.logger.info( f"Successfully loaded modules: { [ self.modules[ module ].get( 'name' ) for module in self.modules ] }" )
+      else:
+        self.logger.critical( "Fail to load modules" )
+        raise ModuleNotFoundError
+
+  def execute_modules( self ):
+    self.logger.info( "Execute modules" )
+    if not self.modules:
+      self.logger.critical( "No modules loaded" )
+      raise ModuleNotFoundError
+
+    executed_modules = list()
+    for module in self.modules:
+      module_name = self.modules[ module ].get( 'name', None )
+      module_path = os.path.join( self.modules_path, module_name )
+      if not os.path.exists( module_path ) or not os.path.isfile( module_path ):
+        self.logger.error( f"Module { module_name } not found" )
+        continue
+
+      ret = Errors.FAILURE.value
+      cmd = f"python3 { module_path } { self.modules[ module ][ 'params' ] }"
+      try:
+        ret = subprocess.check_output( cmd, shell = True, stderr = subprocess.STDOUT )
+      except subprocess.CalledProcessError as exp:
+
+        self.logger.error( f"On starting module { module_name } error occured: { exp.stderr }" )
+      except Exception as exp:
+        self.logger.error( f"Unxepected error: { exp } occurred on calling { module_name }" )
+
+      if ret == Errors.FAILURE.value or ret == Errors.START_FAILURE.value:
+        self.logger.critical( f"{ module_name } failed" )
+        break
+
+      executed_modules.append( module_name )
+
+    if executed_modules:
+      self.logger.info( f"Successfully executed modules: { executed_modules }" )
+    else:
+      self.logger.critical( "Fail to load modules" )
+      raise ModuleExecutionError
+
+  def main( self ):
+    try:
+      self.load_modules()
+    except ( ModuleNotFoundError, Exception ):
+      self.logger.critical( "Fail to complete load user settings" )
+      sys.exit( Errors.FAILURE.value )
+
+    try:
+      self.execute_modules()
+    except Exception as exp:
+      self.logger.critical( f"Fail to complete load user settings. Error: { exp }" )
+      sys.exit( Errors.FAILURE.value )
+    else:
+      self.logger.info( "User setting loader finished." )
+
+if __name__ == '__main__':
+  settings.init()
+  settings_loader = UserSettingLoader()
+  settings_loader.main()
